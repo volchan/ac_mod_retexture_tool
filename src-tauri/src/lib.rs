9768b7a394456ab19b0a9abd6c727fd8c385846c
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2026-04-24 15:16:14 +0200

    feat: persist and restore window size on reopen

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,6 +12,7 @@ pub use commands::texture::Kn5Cache;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
